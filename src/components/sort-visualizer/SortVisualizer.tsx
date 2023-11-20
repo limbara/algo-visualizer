3c7459553be58f75c15a68aa06dc5a57bf4a89e5
limbara <limbaranico@yahoo.com>
2023-11-20 16:24:02 +0000

    chore: delete unused hooks & replace with @restart/hooks

--- src/components/sort-visualizer/SortVisualizer.tsx
+++ src/components/sort-visualizer/SortVisualizer.tsx
@@ -1,73 +1,108 @@
-import { useEffect } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { ActionCreators } from "redux-undo"
-import useElementRect from "../../hooks/useElementRect"
-import { SortVisualizerBoard } from "./SortVisualizerBoard"
-import { selectAlgo, selectArray, selectIsDone, selectIsRunning, selectSpeed, setIsDone, setIsRunning } from "./sortVisualizerSlice"
-import { initArray, setState } from "./sortVisualizerBoardSlice"
-import { SorterEvent, SorterEventDone, SorterEventEnum, SorterEventHighlightItems, SorterEventRunning, SorterEventSwap, sorterFactory } from "./../../algo/interface"
-import { map, takeWhile } from "rxjs"
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { ActionCreators } from "redux-undo";
+import { SortVisualizerBoard } from "./SortVisualizerBoard";
+import {
+  selectAlgo,
+  selectArray,
+  selectIsDone,
+  selectIsRunning,
+  selectSpeed,
+  setIsDone,
+  setIsRunning,
+} from "./sortVisualizerSlice";
+import { initArray, setState } from "./sortVisualizerBoardSlice";
+import {
+  SorterEvent,
+  SorterEventDone,
+  SorterEventEnum,
+  SorterEventHighlightItems,
+  SorterEventRunning,
+  SorterEventSwap,
+  sorterFactory,
+} from "./../../algo/interface";
+import { map, takeWhile } from "rxjs";
+import { useCallbackRef, useResizeObserver } from "@restart/hooks";
 
 export function SortVisualizer() {
-  const selectedSpeed = useSelector(selectSpeed)
-  const selectedArray = useSelector(selectArray)
-  const selectedAlgo = useSelector(selectAlgo)
-  const selectedIsRunning = useSelector(selectIsRunning)
-  const selectedIsDone = useSelector(selectIsDone)
+  const selectedSpeed = useSelector(selectSpeed);
+  const selectedArray = useSelector(selectArray);
+  const selectedAlgo = useSelector(selectAlgo);
+  const selectedIsRunning = useSelector(selectIsRunning);
+  const selectedIsDone = useSelector(selectIsDone);
 
-  let sorter = sorterFactory(selectedArray.slice(), selectedSpeed, selectedAlgo)
+  let sorter = sorterFactory(
+    selectedArray.slice(),
+    selectedSpeed,
+    selectedAlgo
+  );
 
-  const dispatch = useDispatch()
-  const [setRef, domRect] = useElementRect()
+  const dispatch = useDispatch();
+
+  const [wrapperRef, setWrapperRef] = useCallbackRef<HTMLDivElement>();
+  const wrapperRect = useResizeObserver(wrapperRef);
 
   useEffect(() => {
-    dispatch(setIsDone(false))
-    dispatch(initArray(selectedArray.slice()))
+    dispatch(setIsDone(false));
+    dispatch(initArray(selectedArray.slice()));
 
-    sorter = sorterFactory(selectedArray.slice(), selectedSpeed, selectedAlgo)
-  }, [selectedArray, selectedSpeed, selectedAlgo])
+    sorter = sorterFactory(selectedArray.slice(), selectedSpeed, selectedAlgo);
+  }, [selectedArray, selectedSpeed, selectedAlgo]);
 
   const onClick = () => {
-    dispatch(setIsDone(false))
-    dispatch(initArray(selectedArray.slice()))
-    dispatch(ActionCreators.clearHistory())
+    dispatch(setIsDone(false));
+    dispatch(initArray(selectedArray.slice()));
+    dispatch(ActionCreators.clearHistory());
 
-    sorter.sort()
+    sorter
+      .sort()
       .pipe(
         takeWhile((e) => e != null),
-        map(e => e as SorterEvent)
+        map((e) => e as SorterEvent)
       )
-      .subscribe(e => {
+      .subscribe((e) => {
         switch (e.event) {
           case SorterEventEnum.HighlightItems:
-            dispatch(setState({
-              highlightItems: (e as SorterEventHighlightItems).highlightItems
-            }))
-            break
+            dispatch(
+              setState({
+                highlightItems: (e as SorterEventHighlightItems).highlightItems,
+              })
+            );
+            break;
           case SorterEventEnum.SwapItems:
-            dispatch(setState({
-              array: (e as SorterEventSwap).array,
-              highlightItems: (e as SorterEventSwap).highlightItems
-            }))
-            break
+            dispatch(
+              setState({
+                array: (e as SorterEventSwap).array,
+                highlightItems: (e as SorterEventSwap).highlightItems,
+              })
+            );
+            break;
           case SorterEventEnum.Running:
-            dispatch(setIsRunning((e as SorterEventRunning).isRunning))
-            break
+            dispatch(setIsRunning((e as SorterEventRunning).isRunning));
+            break;
           case SorterEventEnum.Done:
-            dispatch(setIsDone((e as SorterEventDone).isDone))
-            break
+            dispatch(setIsDone((e as SorterEventDone).isDone));
+            break;
         }
-      })
-  }
+      });
+  };
 
-  const onClickUndo = () => dispatch(ActionCreators.undo())
-  const onClickRedo = () => dispatch(ActionCreators.redo())
+  const onClickUndo = () => dispatch(ActionCreators.undo());
+  const onClickRedo = () => dispatch(ActionCreators.redo());
 
   return (
-    <div className="w-11/12 h-screen mx-auto mt-2 mb-2" ref={setRef}>
-      {
-        domRect && <SortVisualizerBoard onClick={onClick} onClickRedo={onClickRedo} onClickUndo={onClickUndo} parentWidth={domRect.width} parentHeight={domRect.height} isRunning={selectedIsRunning} isDone={selectedIsDone} />
-      }
+    <div className="w-11/12 h-screen mx-auto mt-2 mb-2" ref={setWrapperRef}>
+      {wrapperRect && (
+        <SortVisualizerBoard
+          onClick={onClick}
+          onClickRedo={onClickRedo}
+          onClickUndo={onClickUndo}
+          parentWidth={wrapperRect.width}
+          parentHeight={wrapperRect.height}
+          isRunning={selectedIsRunning}
+          isDone={selectedIsDone}
+        />
+      )}
     </div>
-  )
+  );
 }
