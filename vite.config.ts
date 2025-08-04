bee57dc6d48969d209f4551969d82a8970379cfe
limbara <limbaranico@yahoo.com>
2025-08-04 09:45:18 +0000

    chore: update vite to the version 7

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/algo-visualizer/' : '/',
-
+  server: {
+    host: true
+  },
   plugins: [react()],
 });
